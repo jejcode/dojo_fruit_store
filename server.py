3161398c880985003e9e7618f6a99b6ae7c482dd
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-03-27 08:45:53 -0600

    added current date/time string to checkout.html

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)  
+from datetime import datetime
 
 @app.route('/')         
 def index():
@@ -10,9 +11,11 @@ def checkout():
     print(request.form)
     # calculate item count before sending it to the page
     total_items = int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])
+    current_date = datetime.now()
+    date = current_date.strftime("%B %d %Y %I:%M:%S %p")
     print(f"Charging {request.form['first_name']} for {total_items} fruit.")
     # sends form data to the web page. Probably not a good idea, but it works for the assignment
-    return render_template("checkout.html", data = request.form, total_items = total_items)
+    return render_template("checkout.html", data = request.form, total_items = total_items, date = date)
 
 @app.route('/fruits')         
 def fruits():
